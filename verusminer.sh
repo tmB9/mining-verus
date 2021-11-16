b77dba2bfa1a3b9eafbd8cb9fdf73c4b9fa4b228
tmB9 <94489956+tmB9@users.noreply.github.com>
2021-11-17 04:52:32 +0700

    Update verusminer.sh

--- verusminer.sh
+++ verusminer.sh
@@ -1,4 +1,3 @@
 # mining-verus
 sudo apt update
-
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvzf nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux- v0.8.2.tar.gz && cd nheqminer && ./nheqminer -v -l na.luckpool.net:3956 -u RBSngAyX2UZrkSJNa9XgwTgQWpmQN3Deis.World1 -px -t 4 d=4096s
